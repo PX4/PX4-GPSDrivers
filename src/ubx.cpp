2efea8ff621788808989d6481cf2de9b02e5c812
Amir Melzer <amir.melzer@mavt.ethz.ch>
2017-05-05 14:37:48 +0200

    ubx: bug fix ubx driver HaE NAV_PVT

--- src/ubx.cpp
+++ src/ubx.cpp
@@ -974,6 +974,7 @@ GPSDriverUBX::payloadRxDone()
 		_gps_position->lat		= _buf.payload_rx_nav_pvt.lat;
 		_gps_position->lon		= _buf.payload_rx_nav_pvt.lon;
 		_gps_position->alt		= _buf.payload_rx_nav_pvt.hMSL;
+		_gps_position->alt_ellipsoid	= _buf.payload_rx_nav_pvt.height;
 
 		_gps_position->eph		= (float)_buf.payload_rx_nav_pvt.hAcc * 1e-3f;
 		_gps_position->epv		= (float)_buf.payload_rx_nav_pvt.vAcc * 1e-3f;
