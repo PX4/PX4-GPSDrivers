a19ded08d11122c3a0ccc745f29e55f1df82b760
kemen209 <kemen209@gmail.com>
2020-10-26 19:39:11 +0800

    replace non-ascii characters in source code
    (cherry picked from commit 04335c2b7ba4a729a33aad8d9b9b025f87347f58)

--- src/sbf.h
+++ src/sbf.h
@@ -36,7 +36,7 @@
  *
  * Septentrio protocol as defined in PPSDK SBF Reference Guide 4.1.8
  *
- * @author Matej Frančeškin <Matej.Franceskin@gmail.com>
+ * @author Matej Franceskin <Matej.Franceskin@gmail.com>
  *
  */
 
@@ -117,22 +117,22 @@
 #pragma pack(push, 1)
 
 typedef struct {
-	uint8_t  mode_type: 4;       /**< Bit ﬁeld indicating the PVT mode type, as follows:
-                                     0: No PVT available (the Error ﬁeld indicates the cause of the absence of the PVT solution)
+    uint8_t  mode_type: 4;       /**< Bit field indicating the PVT mode type, as follows:
+                                     0: No PVT available (the Error field indicates the cause of the absence of the PVT solution)
                                      1: Stand-Alone PVT
                                      2: Differential PVT
                                      3: Fixed location
-                                     4: RTK with ﬁxed ambiguities
-                                     5: RTK with ﬂoat ambiguities
+                                     4: RTK with fixed ambiguities
+                                     5: RTK with float ambiguities
                                      6: SBAS aided PVT
-                                     7: moving-base RTK with ﬁxed ambiguities
-                                     8: moving-base RTK with ﬂoat ambiguities
+                                     7: moving-base RTK with fixed ambiguities
+                                     8: moving-base RTK with float ambiguities
                                      10:Precise Point Positioning (PPP) */
 	uint8_t  mode_reserved: 2;   /**< Reserved */
 	uint8_t  mode_base_fixed: 1; /**< Set if the user has entered the command setPVTMode,base,auto and the receiver
-                                     is still in the process of determining its ﬁxed position. */
-	uint8_t  mode_2d: 1;        /**< 2D/3D ﬂag: set in 2D mode(height assumed constant and not computed). */
-	uint8_t  error;             /**< PVT error code. The following values are deﬁned:
+                                     is still in the process of determining its fixed position. */
+    uint8_t  mode_2d: 1;        /**< 2D/3D flag: set in 2D mode(height assumed constant and not computed). */
+    uint8_t  error;             /**< PVT error code. The following values are defined:
                                      0: No Error
                                      1: Not enough measurements
                                      2: Not enough ephemerides available
@@ -143,54 +143,54 @@ typedef struct {
                                      7: Position output prohibited due to export laws
                                      8: Not enough differential corrections available
                                      9: Base station coordinates unavailable
-                                     10:Ambiguities not ﬁxed and user requested to only output RTK-ﬁxed positions
-                                     Note: if this ﬁeld has a non-zero value, all following ﬁelds are set to their Do-Not-Use value. */
-	double   latitude;          /**< Marker latitude, from −π/2 to +π/2, positive North of Equator */
-	double   longitude;         /**< Marker longitude, from −π to +π, positive East of Greenwich */
-	double   height;            /**< Marker ellipsoidal height (with respect to the ellipsoid speciﬁed by Datum) */
-	float    undulation;        /**< Geoid undulation computed from the global geoid model deﬁned in
-                                     the document ’Technical Characteristics of the NAVSTAR GPS, NATO, June 1991’ */
+                                     10:Ambiguities not fixed and user requested to only output RTK-fixed positions
+                                     Note: if this field has a non-zero value, all following fields are set to their Do-Not-Use value. */
+    double   latitude;          /**< Marker latitude, from -PI/2 to +PI/2, positive North of Equator */
+    double   longitude;         /**< Marker longitude, from -PI to +PI, positive East of Greenwich */
+    double   height;            /**< Marker ellipsoidal height (with respect to the ellipsoid specified by Datum) */
+    float    undulation;        /**< Geoid undulation computed from the global geoid model defined in
+                                     the document 'Technical Characteristics of the NAVSTAR GPS, NATO, June 1991' */
 	float    vn;                /**< Velocity in the North direction */
 	float    ve;                /**< Velocity in the East direction */
 	float    vu;                /**< Velocity in the Up direction */
-	float    cog;               /**< Course over ground: this is deﬁned as the angle of the vehicle with respect
+    float    cog;               /**< Course over ground: this is defined as the angle of the vehicle with respect
                                      to the local level North, ranging from 0 to 360, and increasing towards east.
                                      Set to the do-not-use value when the speed is lower than 0.1m/s. */
-	double   rx_clk_bias;       /**< Receiver clock bias relative to system time reported in the Time System ﬁeld.
+    double   rx_clk_bias;       /**< Receiver clock bias relative to system time reported in the Time System field.
                                      To transfer the receiver time to the system time, use: tGPS/GST=trx-RxClkBias */
 	float    RxClkDrift;        /**< Receiver clock drift relative to system time (relative frequency error) */
 	uint8_t  time_system;       /**< Time system of which the offset is provided in this sub-block:
                                      0:GPStime
                                      1:Galileotime
                                      3:GLONASStime */
-	uint8_t  datum;             /**< This ﬁeld deﬁnes in which datum the coordinates are expressed:
+    uint8_t  datum;             /**< This field defines in which datum the coordinates are expressed:
                                      0: WGS84/ITRS
                                      19: Datum equal to that used by the DGNSS/RTK basestation
                                      30: ETRS89(ETRF2000 realization)
                                      31: NAD83(2011), North American Datum(2011)
-                                     32: NAD83(PA11), North American Datum, Paciﬁcplate (2011)
+                                     32: NAD83(PA11), North American Datum, Pacificplate (2011)
                                      33: NAD83(MA11), North American Datum, Marianas plate(2011)
                                      34: GDA94(2010), Geocentric Datum of Australia (2010)
-                                     250:First user-deﬁned datum
-                                     251:Second user-deﬁned datum */
+                                     250:First user-defined datum
+                                     251:Second user-defined datum */
 	uint8_t  nr_sv;             /**< Total number of satellites used in the PVT computation. */
-	uint8_t  wa_corr_info;      /**< Bit ﬁeld providing information about which wide area corrections have been applied:
+    uint8_t  wa_corr_info;      /**< Bit field providing information about which wide area corrections have been applied:
                                      Bit 0: set if orbit and satellite clock correction information is used
                                      Bit 1: set if range correction information is used
                                      Bit 2: set if ionospheric information is used
                                      Bit 3: set if orbit accuracy information is used(UERE/SISA)
                                      Bit 4: set if DO229 Precision Approach mode is active
                                      Bits 5-7: Reserved */
-	uint16_t reference_id;      /**< In case of DGPS or RTK operation, this ﬁeld is to be interpreted as the base station identiﬁer.
-                                     In SBAS operation, this ﬁeld is to be interpreted as the PRN of the geostationary satellite
+    uint16_t reference_id;      /**< In case of DGPS or RTK operation, this field is to be interpreted as the base station identifier.
+                                     In SBAS operation, this field is to be interpreted as the PRN of the geostationary satellite
                                      used (from 120 to 158). If multiple base stations or multiple geostationary satellites are used
                                      the value is set to 65534.*/
-	uint16_t mean_corr_age;     /**< In case of DGPS or RTK, this ﬁeld is the mean age of the differential corrections.
-                                     In case of SBAS operation, this ﬁeld is the mean age of the ’fast corrections’
+    uint16_t mean_corr_age;     /**< In case of DGPS or RTK, this field is the mean age of the differential corrections.
+                                     In case of SBAS operation, this field is the mean age of the 'fast corrections'
                                      provided by the SBAS satellites */
-	uint32_t signal_info;       /**< Bit ﬁeld indicating the type of GNSS signals having been used in the PVT computations.
+    uint32_t signal_info;       /**< Bit field indicating the type of GNSS signals having been used in the PVT computations.
                                      If a bit i is set, the signal type having index i has been used. */
-	uint8_t alert_flag;         /**< Bit ﬁeld indicating integrity related information */
+    uint8_t alert_flag;         /**< Bit field indicating integrity related information */
 
 	// Revision 1
 	uint8_t nr_bases;
@@ -202,22 +202,22 @@ typedef struct {
 } sbf_payload_pvt_geodetic_t;
 
 typedef struct {
-	uint8_t  mode_type: 4;       /**< Bit ﬁeld indicating the PVT mode type, as follows:
-                                     0: No PVT available (the Error ﬁeld indicates the cause of the absence of the PVT solution)
+    uint8_t  mode_type: 4;       /**< Bit field indicating the PVT mode type, as follows:
+                                     0: No PVT available (the Error field indicates the cause of the absence of the PVT solution)
                                      1: Stand-Alone PVT
                                      2: Differential PVT
                                      3: Fixed location
-                                     4: RTK with ﬁxed ambiguities
-                                     5: RTK with ﬂoat ambiguities
+                                     4: RTK with fixed ambiguities
+                                     5: RTK with float ambiguities
                                      6: SBAS aided PVT
-                                     7: moving-base RTK with ﬁxed ambiguities
-                                     8: moving-base RTK with ﬂoat ambiguities
+                                     7: moving-base RTK with fixed ambiguities
+                                     8: moving-base RTK with float ambiguities
                                      10:Precise Point Positioning (PPP) */
 	uint8_t  mode_reserved: 2;  /**< Reserved */
 	uint8_t  mode_base_fixed: 1;/**< Set if the user has entered the command setPVTMode,base,auto and the receiver
-                                     is still in the process of determining its ﬁxed position. */
-	uint8_t  mode_2d: 1;        /**< 2D/3D ﬂag: set in 2D mode(height assumed constant and not computed). */
-	uint8_t  error;             /**< PVT error code. The following values are deﬁned:
+                                     is still in the process of determining its fixed position. */
+    uint8_t  mode_2d: 1;        /**< 2D/3D flag: set in 2D mode(height assumed constant and not computed). */
+    uint8_t  error;             /**< PVT error code. The following values are defined:
                                      0: No Error
                                      1: Not enough measurements
                                      2: Not enough ephemerides available
@@ -228,8 +228,8 @@ typedef struct {
                                      7: Position output prohibited due to export laws
                                      8: Not enough differential corrections available
                                      9: Base station coordinates unavailable
-                                     10:Ambiguities not ﬁxed and user requested to only output RTK-ﬁxed positions
-                                     Note: if this ﬁeld has a non-zero value, all following ﬁelds are set to their Do-Not-Use value. */
+                                     10:Ambiguities not fixed and user requested to only output RTK-fixed positions
+                                     Note: if this field has a non-zero value, all following fields are set to their Do-Not-Use value. */
 	float cov_vn_vn;            /**< Variance of the north-velocity estimate */
 	float cov_ve_ve;            /**< Variance of the east-velocity estimate */
 	float cov_vu_vu;            /**< Variance of the up - velocity estimate */
@@ -265,25 +265,25 @@ typedef struct {
 
 typedef struct {
 	uint16_t sync;              /** The Sync field is a 2-byte array always set to 0x24, 0x40. The first byte of every SBF block has
-									hexadecimal value 24 (decimal 36, ASCII ’$’). The second byte of every SBF block has hexadecimal
-									value 40 (decimal 64, ASCII ’@’). */
+                                    hexadecimal value 24 (decimal 36, ASCII '$'). The second byte of every SBF block has hexadecimal
+                                    value 40 (decimal 64, ASCII '@'). */
 	uint16_t crc16;				/** The CRC field is the 16-bit CRC of all the bytes in an SBF block from and including the ID field
 									to the last byte of the block. The generator polynomial for this CRC is the so-called CRC-CCITT
 									polynomial: x 16 + x 12 + x 5 + x 0 . The CRC is computed in the forward direction using a seed of 0, no
 									reverse and no final XOR. */
 uint16_t msg_id:
-	13;        /** The ID ﬁeld is a 2-byte block ID, which uniquely identiﬁes the block type and its contents */
+    13;        /** The ID field is a 2-byte block ID, which uniquely identifies the block type and its contents */
 uint8_t msg_revision:
-	3;    /** block revision number, starting from 0 at the initial block deﬁnition, and incrementing
+    3;    /** block revision number, starting from 0 at the initial block definition, and incrementing
                                     each time backwards - compatible changes are performed to the block  */
-	uint16_t length;            /** The Length ﬁeld is a 2-byte unsigned integer containing the size of the SBF block.
+    uint16_t length;            /** The Length field is a 2-byte unsigned integer containing the size of the SBF block.
                                     It is the total number of bytes in the SBF block including the header.
                                     It is always a multiple of 4. */
 	uint32_t TOW;               /**< Time-Of-Week: Time-tag, expressed in whole milliseconds from
                                      the beginning of the current Galileo/GPSweek. */
 	uint16_t WNc;               /**< The GPS week number associated with the TOW. WNc is a continuous
                                      weekcount (hence the "c"). It is not affected by GPS week roll overs,
-                                     which occur every 1024 weeks. By deﬁnition of the Galileo system time,
+                                     which occur every 1024 weeks. By definition of the Galileo system time,
                                      WNc is also the Galileo week number + 1024. */
 	union {
 		sbf_payload_pvt_geodetic_t  payload_pvt_geodetic;
