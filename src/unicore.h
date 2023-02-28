cc90cfc0ebaa5f2e4dfc23a3d3095a31b0d479e7
Julian Oes <julian@oes.ch>
2023-02-28 14:24:55 +1300

    src: add parser for Unicore
    This parser can then be used as part of the NMEA driver to parse Unicore
    specific sentences sent by the Unicore UM982.
    
    Signed-off-by: Julian Oes <julian@oes.ch>

---
+++ src/unicore.h
@@ -0,0 +1,83 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2023 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#pragma once
+
+#include <cstdint>
+
+
+class UnicoreParser
+{
+public:
+	enum class Result {
+		None,
+		WrongCrc,
+		WrongStructure,
+		GotHeading,
+		UnknownSentence,
+	};
+
+	Result parseChar(char c);
+
+	struct Heading {
+		float heading_deg;
+		float baseline_m;
+	};
+
+	Heading heading() const
+	{
+		return _heading;
+	}
+
+
+private:
+	bool crcCorrect() const;
+	bool isHeading() const;
+	bool extractHeading();
+
+
+
+	// We have seen buffers with 154 bytes.
+	char _buffer[256];
+	unsigned _buffer_pos {0};
+	char _buffer_crc[9];
+	unsigned _buffer_crc_pos {0};
+
+	enum class State {
+		Uninit,
+		GotHashtag,
+		GotStar,
+	} _state {State::Uninit};
+
+	Heading _heading{};
+};
